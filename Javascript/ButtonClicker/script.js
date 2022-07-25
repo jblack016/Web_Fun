8ae5250cfcc9ced565739e8ab06fed3551ece6f6
Jared Black <j.black16@icloud.com>
2022-07-25 18:28:36 -0500

    master

---
+++ Javascript/ButtonClicker/script.js
@@ -0,0 +1,17 @@
+
+
+function login(elem){
+if(elem.innerText == "Login"){
+  elem.innerText = "Log Out"
+} else{
+  elem.innerText = "Login"
+}
+}
+
+function def(elem){
+  elem.remove()
+}
+
+function likes(elem){
+  alert("Ninja was liked")
+}
